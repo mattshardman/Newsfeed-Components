e6f19e381f7209d63b4243f21b7f0a6a88583281
mattshardman <mattshardman@gmail.com>
2019-01-30 15:02:10 +0000

    update styling and add transitions

--- Article/Article.js
+++ Article/Article.js
@@ -6,15 +6,18 @@ class Article {
     this.domElement = domElement;
     // create a reference to the ".expandButton" class.
     this.expandButton = domElement.querySelector('.expandButton');
+    this.arrow = domElement.querySelector('.expandButton .material-icons');
+    this.content = domElement.querySelector('.content');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.arrow.textContent = 'expand_more';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open');
+    this.content.classList.toggle('content-open');
+    this.arrow.classList.toggle('arrow-open');
   }
 }
 
